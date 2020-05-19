c7f0f4c49b0c30df5662e4cb2ba34513d877216e
Joseph Lee <illumeweb@gmail.com>
2020-05-19 14:02:07 +0900

    auth

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,10 +1,27 @@
-import { BaseService } from '../../core';
+import { BaseService, NanudaException } from '../../core';
 import { Injectable } from '@nestjs/common';
+import { PasswordService } from './password.service';
+import { AdminLoginDto } from './dto';
+import { Admin } from '../admin';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class AuthService extends BaseService {
-    constructor() {
+    constructor(
+        private readonly passwordService: PasswordService,
+        @InjectRepository(Admin) private readonly adminRepo: Repository<Admin>
+    ) {
         super()
+    }
+
+    async adminLogin(adminLoginDto: AdminLoginDto): Promise<Admin> {
+        const admin = await this.adminRepo.findOne({ phone: adminLoginDto.phone });
+        if (!admin) {
+            throw new NanudaException('auth.notFound')
+        }
+        return admin;
+
     }
 
 }
